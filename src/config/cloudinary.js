7fcee7736b2c78f4aa35995a47eaf88d1e8aa196
Arun_Kumar <112682836+Arun-kumar-95@users.noreply.github.com>
2023-02-22 16:49:33 +0530

    Update cloudinary.js

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,11 @@
+// require the cloudinary 
 const cloudinary = require("cloudinary").v2;
-
+// setting the cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_KEY,
   api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
+// exporting the cloudinary
 module.exports = cloudinary;
