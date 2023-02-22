fb7aa2bf8c4738fc85d5ee630d0d663b9bacff1a
Arun_Kumar <112682836+Arun-kumar-95@users.noreply.github.com>
2023-02-22 17:28:38 +0530

    Update main.js

--- src/server/main.js
+++ src/server/main.js
@@ -1,16 +1,22 @@
+// require the express
 const express = require("express");
+// assigning the express function to app variable
 const app = express();
+// require the dotenv file
 const dotenv = require("dotenv");
+// if not an env = produuction then 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({ path: "./src/config/config.env" });
 }
+// destructure the connect function
 const { connect } = require("../config/config.db.js");
 
+// logging the message based on connect function outputs
 if (connect(process.env.DATABASE_URL)) {
   // connect the server if we have database connected
   app.listen(process.env.PORT, () => {
     console.log("listening on port " + process.env.PORT);
   });
 }
-
+// exporting the app function
 module.exports = app;
