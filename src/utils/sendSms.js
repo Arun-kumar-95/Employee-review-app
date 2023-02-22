1b92d8459e5cad3b9c3f21fadd487e3896a9fc08
Arun_Kumar <112682836+Arun-kumar-95@users.noreply.github.com>
2023-02-22 17:32:58 +0530

    Update sendSms.js

--- src/utils/sendSms.js
+++ src/utils/sendSms.js
@@ -1,12 +1,14 @@
+// require the twilio package
 const twilio = require("twilio");
 
+// exporting the send sms function
 module.exports.sendSms = (BODY, PHONE) => {
   const client = new twilio(
     process.env.TWILIO_SID,
     process.env.TWILIO_AUTH_TOKEN
   );
 
- 
+//  return the client message function 
   return client.messages
     .create({
       body: `${BODY}`,
