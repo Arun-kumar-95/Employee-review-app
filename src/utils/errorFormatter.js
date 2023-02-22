a248158429d4615e9cbd18ab831d5d5a1a018d94
Arun_Kumar <112682836+Arun-kumar-95@users.noreply.github.com>
2023-02-22 17:31:30 +0530

    Update errorFormatter.js

--- src/utils/errorFormatter.js
+++ src/utils/errorFormatter.js
@@ -1,12 +1,18 @@
+// exporting the error formatter function
 module.exports.errorFormatter = (e) => {
+//   let error array ro be empty
   let errors = [];
+// separating th error from  ":"  
   const allErrors = e.substring(e.indexOf(":") + 1).trim();
+  // separating th error msg from  ","  
   const allErrorsInArrayFormat = allErrors.split(",").map((err) => err.trim());
 
+// applying for each to error   
   allErrorsInArrayFormat.forEach((err) => {
     const [key, value] = err.split(":").map((err) => err.trim());
     errors.push(value);
   });
 
+//   return the error
   return errors;
 };
